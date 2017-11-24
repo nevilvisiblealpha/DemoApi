8f1cae519b8da04ed4fb4fb44a8f229e080b4948
nevil.panchal <nevil.panchal@visiblealpha.com>
2017-11-24 22:20:45 +0530

    Log file read and request changes

--- src/test/java/FrameWork/JsonTests.java
+++ src/test/java/FrameWork/JsonTests.java
@@ -2,27 +2,35 @@ package FrameWork;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.config.SSLConfig;
+import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Iterator;
 
 public class JsonTests {
 
-    @Test
-    public  void ApiExecuter() throws Throwable {
+    @Test(dataProvider ="Base Urls provider")
+    public  void ApiExecuter(String basePath) throws Throwable {
         try {
             Reporter.log("*************Expected Response As Below :************************",true);
-            Response ExpectedResponse = getQuery("http://date.jsontest.com/");
-
+            Response ExpectedResponse = getQuery("http://uat-webdq.visiblealpha.com:81",basePath);
+//"http://uat-webdq.visiblealpha.com:81
 
             Reporter.log("*************Actual Response As Below  :***************************",true);
-            Response ActualResponse = getQuery("http://date.jsontest.com/");
+            Response ActualResponse = getQuery("http://uat-webdq.visiblealpha.com:81",basePath);
+            if(ExpectedResponse.getStatusCode()==200||ActualResponse.getStatusCode()==200 ) {
+                JSONAssert.assertEquals(ExpectedResponse.asString(), ActualResponse.asString(), JSONCompareMode.NON_EXTENSIBLE);
+                Reporter.log("***************Passed -Json output Matched**********************************",true);
+            }else{
+                throw new Exception("Not 200 request");
+            }
 
-            JSONAssert.assertEquals(ExpectedResponse.asString(), ActualResponse.asString(), JSONCompareMode.NON_EXTENSIBLE);
-            Reporter.log("***************Passed -Json output Matched**********************************",true);
         }catch(AssertionError e)
         {
 
@@ -33,23 +41,24 @@ public class JsonTests {
             StringBuffer HTMLTableString = new StringBuffer();
             for(String message:ErrorMessages)
             {
-                   String   messageCaption = message.split("\n")[0];
-                   String expectedValue = message.split("\n")[1].split(": ")[1];
-                String actualValue = message.split("\n")[2].split(": ")[1];
+                if(message.contains("got")) {
+                    String messageCaption = message.split("\n")[0];
+                    String expectedValue = message.split("\n")[1].split(": ")[1];
+                    String actualValue = message.split("\n")[2].split(": ")[1];
 
-               stringBuilder = "<table>\n" +
-                        "  <caption>"+messageCaption+"</aption>\n" +
-                        "  <tr>\n" +
-                        "    <th>Expected</th>\n" +
-                        "    <th>Actual Found</th>\n" +
-                        "  </tr>\n" +
-                        "  <tr>\n" +
-                        "    <td>"+expectedValue+"</td>\n" +
-                        "    <td>"+actualValue+"</td>\n" +
-                        "  </tr>\n" +
-                        "</table>\n";
-                 HTMLTableString.append(stringBuilder);
-
+                    stringBuilder = "<table>\n" +
+                            "  <caption>" + messageCaption + "</aption>\n" +
+                            "  <tr>\n" +
+                            "    <th>Expected</th>\n" +
+                            "    <th>Actual Found</th>\n" +
+                            "  </tr>\n" +
+                            "  <tr>\n" +
+                            "    <td>" + expectedValue + "</td>\n" +
+                            "    <td>" + actualValue + "</td>\n" +
+                            "  </tr>\n" +
+                            "</table>\n";
+                    HTMLTableString.append(stringBuilder);
+                }
 
             }
 
@@ -80,19 +89,33 @@ public class JsonTests {
     }
 
 
-    public static Response getQuery(String URL) throws Throwable {
-
+    public static Response getQuery(String URL,String basePath) throws Throwable {
+        String basePaths = basePath.split("\\?")[0];
+        String params = basePath.split("\\?")[1];
         RestAssured.baseURI = URL;
-        // RestAssured.basePath ="/users/sign_in";
+       RestAssured.basePath =basePaths;
 
 
-            Response response = RestAssured.given().config(RestAssured.config().sslConfig(
-                    new SSLConfig().relaxedHTTPSValidation())).contentType(com.jayway.restassured.http.ContentType.JSON).
+            Response response = RestAssured.given().basePath(basePaths).config(RestAssured.config().sslConfig(
+                    new SSLConfig().relaxedHTTPSValidation())).contentType(ContentType.JSON).
                     when()
-                    .get();
+                    .get("?"+params);
+
            Reporter.log(response.asString(),true);
 
 
         return response;
     }
+
+
+
+    @DataProvider(name ="Base Urls provider")
+    public Iterator<Object[]> getDataproviderValue()
+    {
+        LogFileReader logfile = new LogFileReader();
+        return logfile.fileReader();
+    }
+
+
+
 }
---
+++ src/test/java/FrameWork/LogFileReader.java
@@ -0,0 +1,42 @@
+package FrameWork;
+
+import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class LogFileReader {
+
+
+
+    public Iterator<Object[]> fileReader() {
+        Set<Object[]> basePaths = new HashSet<Object[]>();
+        try {
+            FileInputStream fstream = new FileInputStream("D:\\DemoAPI\\DemoApi\\src\\test\\resources\\webdq_access.log");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+           /* read log line by line */
+            while ((strLine = br.readLine()) != null) {
+             /* parse strLine to obtain what you want */
+                //System.out.println(strLine);
+
+                if(strLine.contains("GET"))
+                {
+
+                   String line = strLine.split("GET ")[1].split(" ")[0];
+                    System.out.println(line);
+                    basePaths.add(new Object[]{line});
+                }
+
+            }
+            fstream.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+      return basePaths.iterator();
+    }
+}
